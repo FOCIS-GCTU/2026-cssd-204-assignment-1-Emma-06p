c127be4317754fe12d984f96eaeaba1a8d71d1a6
Emma-06p <2425403806@live.gctu.edu.gh>
2026-05-31 03:17:58 -0800

    Add files via upload

---
+++ EAN.py.py
@@ -0,0 +1,36 @@
+# File: EAN.py
+# Description: Print out my initials
+# Assignment number: 2
+# Name: Emmanuel Ampomah Nana
+# Student ID: 2425403806
+# Email: emmaampomah0609@gmail.com
+# Grader: Mr.Augustus Buckman
+# On my honor, Emmanuel Ampomah Nana, this programming work was made by me no one else.
+# And I have not provided this assignment to anyone.
+
+def main():
+
+    print()
+
+    print("...EAN")
+
+    print()
+
+    print("...EEEEEEEEEEEE........AAAAAAAAAAAA........NN........NN...")
+    print("...EEE.................AAA......AAA........NN.N......NN...")
+    print("...EEE.................AAA......AAA........NN..N.....NN...")
+    print("...EEE.................AAA......AAA........NN...N....NN...")
+    print("...EEEEEE..............AAAAAAAAAAAA........NN....N...NN...")
+    print("...EEE.................AAA......AAA........NN.....N..NN...")
+    print("...EEE.................AAA......AAA........NN.......NNN...")
+    print("...EEE............**...AAA......AAA...**...NN........NN...")
+    print("...EEEEEEEEEEEE...**...AAA......AAA...**...NN........NN...")
+
+    print()
+
+
+main()
+
+
+
+
